ee66f8223265510be800271d5816103da0e6cb0a
lichunyang <958749178@qq.com>
2018-12-14 16:24:46 +0800

    完成model设计

---
+++ MxOnline/apps/courses/models.py
@@ -0,0 +1,52 @@
+from datetime import datetime
+
+from django.db import models
+
+# Create your models here.
+
+class Course(models.Model):
+    name = models.CharField(max_length=100, verbose_name='课程名')
+    desc = models.CharField(max_length=300, verbose_name='描述')
+    degree = models.CharField(choices=(('cj', '初级'), ('zj', '中级'), ('gj', '高级')), max_length=2, verbose_name='课程难度')
+    learn_nums = models.IntegerField(default=0, verbose_name='学习人数')
+    dev_nums = models.IntegerField(default=0, verbose_name='收藏人数')
+    learn_times = models.IntegerField(default=0, verbose_name='学习时长（分钟数）')
+    click_nums = models.IntegerField(default=0, verbose_name='点击数')
+    detail = models.TextField(verbose_name='课程详情')
+    image = models.ImageField(upload_to='courses/%Y/%m', verbose_name='课程图片')
+    add_time = models.DateTimeField(default=datetime.now, verbose_name='添加时间')
+
+    class Meta:
+        verbose_name = '课程'
+        verbose_name_plural = verbose_name
+
+
+class Lesson(models.Model):
+    course = models.ForeignKey(Course, verbose_name='课程')
+    name = models.CharField(max_length=100, verbose_name='章节')
+    add_time = models.DateTimeField(default=datetime.now, verbose_name='添加时间')
+
+    class Meta:
+        verbose_name = '课程章节'
+        verbose_name_plural = verbose_name
+
+
+class Video(models.Model):
+    lesson = models.ForeignKey(Course, verbose_name='章节')
+    name = models.CharField(max_length=100, verbose_name='视频')
+    add_time = models.DateTimeField(default=datetime.now, verbose_name='添加时间')
+
+    class Meta:
+        verbose_name = '课程视频'
+        verbose_name_plural = verbose_name
+
+
+class CourseResource(models.Model):
+    course = models.ForeignKey(Course, verbose_name='课程')
+    name = models.CharField(max_length=100, verbose_name='视频')
+    download = models.FileField(upload_to='courses/resource/%Y/%m', verbose_name='资源文件', max_length=100)
+    add_time = models.DateTimeField(default=datetime.now, verbose_name='添加时间')
+
+    class Meta:
+        verbose_name = '课程资源'
+        verbose_name_plural = verbose_name
---
+++ MxOnline/apps/users/models.py
@@ -0,0 +1,51 @@
+from datetime import datetime
+
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+
+class UserProfile(AbstractUser):
+    nick_name = models.CharField(max_length=50, verbose_name="昵称", default='')
+    # blank表示表单验证， null表示数据范畴
+    birthday = models.DateField(verbose_name='生日', null=True, blank=True)
+    gender = models.CharField(max_length=100, choices=(('male', '男'), ('female', '女')), default='female')
+    address = models.CharField(max_length=100, default='')
+    mobile = models.CharField(max_length=11, null=True, blank=True)
+    # ImageField 需要调用Pillow库
+    image = models.ImageField(upload_to='image/%Y/%m', default='image/default.png', max_length=100)
+
+    class Meta:    # 问题这个类什么时候被调用，他的作用是什么？ 在admin界面显示的文字
+        '''
+        verbose_name:对象的一个易于理解的名称，为单数：
+        如果此项没有设置，Django会把类名拆分开来作为自述名，比如CamelCase 会变成camel case，
+        verbose_name_plural: 该对象复数形式的名称：
+        '''
+        verbose_name = '用户信息'
+        verbose_name_plural = verbose_name
+
+    def __str__(self):
+        return self.username
+
+
+class EmailVerifyRecord(models.Model):
+    code = models.CharField(max_length=10, verbose_name='验证码')
+    email = models.EmailField(max_length=30, verbose_name='邮箱')
+    send_type = models.CharField(choices=(('register', '登录'), ('forget', '找回密码')), max_length=100)
+    # datetime.now不能带括号 带括号是实例化时的时间
+    send_time = models.DateTimeField(default=datetime.now)
+
+    class Meta:
+        verbose_name = '邮箱验证码'
+        verbose_name_plural = verbose_name
+
+
+class Banner(models.Model):
+    title = models.CharField(max_length=100, verbose_name='标题')
+    image = models.ImageField(upload_to='banner/%Y/%m', max_length=100, verbose_name='轮播图')
+    url = models.URLField(max_length=100, verbose_name='访问地址')
+    index = models.IntegerField(verbose_name='访问顺序')
+    add_time = models.DateTimeField(default=datetime.now, verbose_name='添加时间')
+
+    class Meta:
+        verbose_name = '轮播图'
+        verbose_name_plural = verbose_name
